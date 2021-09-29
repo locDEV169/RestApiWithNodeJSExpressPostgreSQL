bd49406ab74408a7c3b898874ad85658e3355fe2
locDEV169 <loc16910@gmail.com>
2021-09-29 15:37:55 +0700

    update #3

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const UserRouters = require('./src/user/routes');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/",(req,res) => {
+    res.send("Hello world <3")
+})
+
+app.use('/api/users',UserRouters);
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT,() => {
+    const port = server.address().port;
+    console.log(`app listening on port ${port}`)
+})
